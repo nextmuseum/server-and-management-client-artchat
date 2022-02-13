d4cfb7a6269dc131478d68e2209b847d9b09f83a
Alexander Buhler <alexander.buhler@projekteins.biz>
2022-02-13 23:02:01 +0100

    fix auth headers / allow report filtered objects

--- api/server.js
+++ api/server.js
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
     // Request methods to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     // Request headers to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization')
 
     next()
 })
