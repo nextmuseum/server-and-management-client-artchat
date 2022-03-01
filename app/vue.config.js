4faf0366d416c9f9212b5816c56cd027a2426167
Alexander Buhler <alexander.buhler@projekteins.biz>
2022-03-01 20:17:05 +0100

    update app build config #2

--- app/vue.config.js
+++ app/vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = defineConfig({
   outputDir: path.resolve(__dirname, "../api/app"),
-  publicPath: (process.env.NODE_ENV === 'production')
+  publicPath: (process.env.NODE_ENV !== 'local')
     ? '/app/'
     : '/',
   transpileDependencies: true,
