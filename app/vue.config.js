a43de333214d51ae8f396c0e5e3a52c11c01b83e
Alexander Buhler <alexander.buhler@projekteins.biz>
2022-07-31 20:18:10 +0200

    reconfigure vue build config

--- app/vue.config.js
+++ app/vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require("path");
 
 module.exports = defineConfig({
-  mode: 'production',
   outputDir: path.resolve(__dirname, "../api/app"),
   publicPath: (process.env.NODE_ENV !== 'local')
     ? '/app/'
